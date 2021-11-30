b0babd850a2c8217444e00bd2929bbcf947315d5
YuanYuan <yuanyuan1992819@126.com>
2021-11-30 18:07:24 +0800

    updates

--- example.cpp
+++ example.cpp
@@ -1,11 +1,41 @@
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) {
-    return i + j;
+#define _def(func) m.def(#func, &func, #func)
+
+using namespace std;
+
+int add_int(int a, int b) {
+    return a + b;
+}
+
+double add_double(double a, double b) {
+    return a + b;
+}
+
+bool and_bool(bool a, bool b) {
+    return a && b;
+}
+
+int64_t add_int64_t(int64_t a, int64_t b) {
+    return a + b;
+}
+
+string test_string(const string &a) {
+    return a + a;
+}
+
+string test_const_char(const char * a) {
+    return test_string(string(a));
 }
 
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
-    m.def("add", &add, "A function which adds two numbers");
+    m.def("add_int", &add_int, "add_int");
+    m.def("add_double", &add_double, "add_double");
+    _def(and_bool);
+    _def(add_int64_t);
+    _def(test_string);
+    _def(test_const_char);
+
 }
--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, Extension
 functions_module = Extension(
     name='example',
     sources=['example.cpp'],
-    extra_compile_args=["-O3","-fPIC"]
+    extra_compile_args=["-O3","-fPIC"],
+    include_dirs=[r'C:\Users\user\AppData\Local\Programs\Python\Python38\Lib\site-packages\pybind11\include']
 )
 
 setup(ext_modules=[functions_module])
